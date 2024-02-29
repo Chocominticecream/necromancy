ac9ac65ee9aa6aa91f1db4229af2a707e8865b0a
Chocominticecream <weihao.kimmers@gmail.com>
2024-02-29 23:56:20 +0800

    main menu UI overhaul
    the UI overhaul starts with the main menu, now it looks way more stylish

---
+++ scenes/levels/MainMenuBgAnimations.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+#tween variables
+var moonTween
+var pivotTween
+
+
+@onready var moonSprite = $Moon
+@onready var blinkingStars = $Stars
+@onready var pivotAccessory = $danglingAccessory/pivot
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    moonFloat()
+    pivotAnimation()
+    blinkingStars.explosiveness = 1.0
+    await get_tree().create_timer(0.05).timeout;
+    blinkingStars.explosiveness = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+
+func moonFloat():
+    moonTween = create_tween().set_loops()
+    moonTween.tween_property(moonSprite, "global_position", moonSprite.global_position + Vector2(0,20), 3.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+    moonTween.tween_property(moonSprite, "global_position", moonSprite.global_position - Vector2(0,20), 3.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+
+func pivotAnimation():
+    moonTween = create_tween().set_loops()
+    moonTween.tween_property(pivotAccessory, "rotation", 0.1, 5.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+    moonTween.tween_property(pivotAccessory, "rotation", -0.1, 5.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+    
---
+++ scenes/widgets/BaseButton.gd
@@ -0,0 +1,27 @@
+extends Button
+
+#ffffcd color code for highlighted text?
+
+@export var bbText : String
+@onready var textLabel = $TextLabel
+
+var initialTextPos
+var textTween
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    initialTextPos =  textLabel.global_position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+
+
+func _on_mouse_entered():
+    textTween = create_tween()
+    textTween.tween_property(textLabel, "global_position", initialTextPos + Vector2(20,0), 0.2).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+
+
+func _on_mouse_exited():
+    textTween = create_tween()
+    textTween.tween_property(textLabel, "global_position", initialTextPos, 0.2).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
